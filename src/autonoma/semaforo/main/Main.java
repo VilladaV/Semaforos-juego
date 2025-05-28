8b6bcf6d2d4722f9a81022c54b1bb51aa3516a3e
Juan Esteban Giraldo Betancur <juanesgiraldobetancur@gmail.com>
2025-05-28 10:16:56 -0500

    Agregando Documentacion

--- src/autonoma/semaforo/main/Main.java
+++ src/autonoma/semaforo/main/Main.java
@@ -1,13 +1,24 @@
 package autonoma.semaforo.main;
 
-// * @author : Jhoan Andres Villada - Juan Esteban Giraldo Betancur - Isabela Quintero Murillo
-// * @since 26/05/25
-// * @version 1.0.0
-
 import javax.swing.*;
 
+/**
+ * Clase principal que lanza el menú principal del simulador de tráfico.
+ * Inicia la interfaz gráfica usando `SwingUtilities`.
+ * 
+ * @author 
+ *     Juan Esteban - Isabela - Jhojan
+ */
 public class Main {
+
+    /**
+     * Método principal de la aplicación.
+     * Lanza el menú principal en el hilo de eventos de Swing.
+     * 
+     * @param args Argumentos de línea de comandos (no utilizados).
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new MenuPrincipal());
     }
-} 
+}
+
