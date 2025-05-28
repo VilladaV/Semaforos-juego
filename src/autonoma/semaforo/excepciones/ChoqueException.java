f734b9821399aadf48519acf8051eefd94da4953
Juan Esteban Giraldo Betancur <juanesgiraldobetancur@gmail.com>
2025-05-28 10:10:17 -0500

    Agregando Documentacion

--- src/autonoma/semaforo/excepciones/ChoqueException.java
+++ src/autonoma/semaforo/excepciones/ChoqueException.java
@@ -1,34 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package autonoma.semaforo.excepciones;
 
-// * @author : Jhoan Andres Villada - Juan Esteban Giraldo Betancur - Isabela Quintero Murillo
-// * @since 26/05/25
-// * @version 1.0.0
-
+/**
+ * Excepción personalizada que se lanza cuando dos vehículos colisionan en la intersección.
+ * Guarda información sobre los carriles involucrados y la hora del choque.
+ * 
+ * @author 
+ *     Juan Esteban - Isabela - Jhojan
+ */
 public class ChoqueException extends Exception {
+
     private final String carril1;
     private final String carril2;
     private final String horaChoque;
 
+    /**
+     * Constructor de la excepción.
+     * 
+     * @param carril1 Carril del primer vehículo involucrado.
+     * @param carril2 Carril del segundo vehículo involucrado.
+     */
     public ChoqueException(String carril1, String carril2) {
-        super("¡CHOQUE DETECTADO! Entre carril " + carril1 + " y " + carril2 + 
-              "\nHora: " + java.time.LocalTime.now().format(java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss")));
+        super("¡CHOQUE DETECTADO! Entre carril " + carril1 + " y " + carril2 +
+              "\nHora: " +
+              java.time.LocalTime.now().format(java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss")));
         this.carril1 = carril1;
         this.carril2 = carril2;
-        this.horaChoque = java.time.LocalTime.now().format(java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss"));
+        this.horaChoque =
+            java.time.LocalTime.now().format(java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss"));
     }
 
+    /**
+     * Obtiene el carril del primer vehículo.
+     * 
+     * @return carril1
+     */
     public String getCarril1() {
         return carril1;
     }
 
+    /**
+     * Obtiene el carril del segundo vehículo.
+     * 
+     * @return carril2
+     */
     public String getCarril2() {
         return carril2;
     }
-    
+
+    /**
+     * Obtiene la hora en que ocurrió el choque.
+     * 
+     * @return hora del choque.
+     */
     public String getHoraChoque() {
         return horaChoque;
     }
