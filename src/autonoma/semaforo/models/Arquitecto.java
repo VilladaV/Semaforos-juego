71e58f9bdc30e787e410f9eb1beaef8e10af6dcd
Juan Esteban Giraldo Betancur <juanesgiraldobetancur@gmail.com>
2025-05-28 10:23:20 -0500

    Agregando Documentacion

--- src/autonoma/semaforo/models/Arquitecto.java
+++ src/autonoma/semaforo/models/Arquitecto.java
@@ -6,9 +6,20 @@ package autonoma.semaforo.models;
 
 import javax.swing.ImageIcon;
 import java.util.Random;
-
+/**
+ * Clase que representa un personaje del tipo Arquitecto.
+ * Este personaje se mueve de forma horizontal sobre la pantalla.
+ * Extiende de la clase base {@link Personaje}.
+ * 
+ * @author 
+ *     Juan Esteban - Isabela - Jhojan
+ */
 public class Arquitecto extends Personaje {
-
+ /**
+     * Constructor que inicializa la posición y la imagen del arquitecto.
+     * 
+     * @param imagen Imagen asociada al personaje arquitecto.
+     */
     public Arquitecto(ImageIcon imagen) {
         super(320, 410 + new Random().nextInt(20), 1, Direccion.HORIZONTAL, imagen);
     }
