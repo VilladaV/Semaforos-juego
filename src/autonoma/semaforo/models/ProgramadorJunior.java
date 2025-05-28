464c8b0be1e24b865e0ac8c3f3477f57708c987f
Juan Esteban Giraldo Betancur <juanesgiraldobetancur@gmail.com>
2025-05-28 10:39:32 -0500

    Agregando Documentacion

--- src/autonoma/semaforo/models/ProgramadorJunior.java
+++ src/autonoma/semaforo/models/ProgramadorJunior.java
@@ -3,12 +3,21 @@ package autonoma.semaforo.models;
 import javax.swing.ImageIcon;
 import java.util.Random;
 
-// * @author : Jhoan Andres Villada - Juan Esteban Giraldo Betancur - Isabela Quintero Murillo
-// * @since 27/05/25
-// * @version 1.0.0
-
+/**
+ * Clase que representa un personaje del tipo Programador Junior.
+ * Este personaje se mueve en dirección vertical desde la parte inferior de la pantalla hacia arriba.
+ * Hereda de la clase {@link Personaje}.
+ * 
+ * @author 
+ *     Juan Esteban - Isabela - Jhojan
+ */
 public class ProgramadorJunior extends Personaje {
 
+    /**
+     * Constructor que inicializa la posición aleatoria y la imagen del programador junior.
+     * 
+     * @param imagen Imagen asociada al personaje.
+     */
     public ProgramadorJunior(ImageIcon imagen) {
         super(320 + new Random().nextInt(40), 800, 1, Direccion.VERTICAL, imagen);
     }
