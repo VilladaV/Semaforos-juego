1ed789ab759e1aab0e714b127957863d32d63b63
Juan Esteban Giraldo Betancur <juanesgiraldobetancur@gmail.com>
2025-05-28 10:07:37 -0500

    Agregando Documentacion

--- src/autonoma/semaforo/excepciones/AtropelloException.java
+++ src/autonoma/semaforo/excepciones/AtropelloException.java
@@ -1,15 +1,34 @@
 package autonoma.semaforo.excepciones;
 
+/**
+ * Excepción personalizada que se lanza cuando un vehículo atropella a un personaje en el juego.
+ * 
+ * @author 
+ *     Juan Esteban - Isabela - Jhojan
+ */
 public class AtropelloException extends Exception {
+
     private final String mensaje;
 
+    /**
+     * Constructor de la excepción.
+     * 
+     * @param personaje El tipo de personaje atropellado.
+     * @param carril    El carril donde ocurrió el atropello.
+     */
     public AtropelloException(String personaje, String carril) {
         super("¡ATROPELLO DETECTADO!\nPersonaje: " + personaje + "\nCarril: " + carril);
         this.mensaje = "¡ATROPELLO DETECTADO!\nPersonaje: " + personaje + "\nCarril: " + carril;
     }
 
+    /**
+     * Obtiene el mensaje de error personalizado.
+     * 
+     * @return mensaje del atropello.
+     */
     public String getMensaje() {
         return mensaje;
     }
 }
+
 
